cae0e52588799016e59411d547805c903883bc3b
MatevzVidovic <matevz.vidovic@gmail.com>
2024-04-04 23:25:55 +0200

    Dela

--- model.py
+++ model.py
@@ -11,6 +11,17 @@ from sklearn.metrics import accuracy_score, classification_report
 
 printout = False
 
+test_purposes = True
+
+
+
+
+
+
+
+
+
+
 
 df = pd.read_csv("dataFrameOfImportants.csv")
 
@@ -29,15 +40,16 @@ if printout:
 
 # make a dictionary between unique values and numbers, and transform y so it is a vector of corresponding numbers
 category2hash = {}
+categories = []
 for i, category in enumerate(y.unique()):
     category2hash[category] = i
+    categories.append(category)
 
 y = y.apply(lambda x: category2hash[x])
 
 if printout:
     print(y.unique())
     print(y)
-
 
 
 
@@ -51,6 +63,13 @@ vectorizer = TfidfVectorizer()
 X_train_TF_IDF = vectorizer.fit_transform(X_train['HeadlineAndDesc'])
 X_test_TF_IDF = vectorizer.transform(X_test['HeadlineAndDesc'])
 
+if test_purposes:
+    train_rows = 1000
+    test_rows = 1000
+    X_train_TF_IDF = X_train_TF_IDF[:train_rows, :]
+    y_train = y_train[:train_rows]
+    X_test_TF_IDF = X_test_TF_IDF[:test_rows, :]
+    y_test = y_test[:test_rows]
 
 if printout:
     
@@ -139,16 +158,57 @@ X_test_coo = X_test_TF_IDF_trimmed.tocoo()
 
 
 
+"""
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+TU MORA BIT
+dtype=torch.float32
+KER PYTORCH V NN.LINEAR UPORABLJA FLOAT32, V OSNOVI SO PA TO FLOAT64 (DOUBLE)
+
+https://discuss.pytorch.org/t/runtimeerror-mat1-and-mat2-must-have-the-same-dtype/166759/7
+
+IN DOBIM TA ERROR:
+Epoch 1
+-------------------------------
+Traceback (most recent call last):
+  File "/home/matevzvidovic/Desktop/SeminarskaDemo/model.py", line 420, in <module>
+    train(train_dataloader, model, loss_fn, optimizer)
+  File "/home/matevzvidovic/Desktop/SeminarskaDemo/model.py", line 367, in train
+    pred = model(X)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
+    return self._call_impl(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
+    return forward_call(*args, **kwargs)
+  File "/home/matevzvidovic/Desktop/SeminarskaDemo/model.py", line 333, in forward
+    logits = self.linear_relu_stack(x)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
+    return self._call_impl(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
+    return forward_call(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/container.py", line 217, in forward
+    input = module(input)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
+    return self._call_impl(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
+    return forward_call(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/linear.py", line 116, in forward
+    return F.linear(input, self.weight, self.bias)
+RuntimeError: mat1 and mat2 must have the same dtype, but got Double and Float
+
+
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+"""
+
 i = np.vstack((X_train_coo.row, X_train_coo.col))
 v = X_train_coo.data
-X_train_tens = torch.sparse_coo_tensor(i, v, X_train_coo.shape)
+X_train_tens = torch.sparse_coo_tensor(i, v, X_train_coo.shape, dtype=torch.float32)
 
 i = np.vstack((X_test_coo.row, X_test_coo.col))
 v = X_test_coo.data
-X_test_tens = torch.sparse_coo_tensor(i, v, X_test_coo.shape)
+X_test_tens = torch.sparse_coo_tensor(i, v, X_test_coo.shape, dtype=torch.float32)
 
-y_train_tens = torch.tensor(y_train.values.astype("int8"))
-y_test_tens = torch.tensor(y_test.values.astype("int8"))
+y_train_tens = torch.tensor(y_train.values.astype("int8")).type(torch.LongTensor)
+y_test_tens = torch.tensor(y_test.values.astype("int8")).type(torch.LongTensor)
 
 
 
@@ -285,3 +345,244 @@ for X, y in test_dataloader:
     print(f"Shape of X [N, C, H, W]: {X.shape}")
     print(f"Shape of y: {y.shape} {y.dtype}")
     break
+
+
+
+# Get cpu, gpu or mps device for training.
+device = (
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
+print(f"Using {device} device")
+
+# Define model
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.flatten = nn.Flatten()
+        
+        """
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        PREJ SEM IMEL NA ZADNJEM
+        nn.Linear(300, 14)
+
+        DOBIL SEM PODOBNO NAPAKO KOT SPODAJ. PO NASVETU SEM POGNAL KOT
+        CUDA_LAUNCH_BLOCKING=1 python3 model.py
+        PA JE BILA ISTA NAPAKA.
+
+        KOT KAZE SEM MISMATCHAL STEVILO TARGET VARIABLEOV
+
+        https://discuss.pytorch.org/t/runtimeerror-cuda-error-device-side-assert-triggered/34213/9
+        
+        https://discuss.pytorch.org/t/how-to-fix-cuda-error-device-side-assert-triggered-error/137553
+
+
+        Epoch 1
+-------------------------------
+../aten/src/ATen/native/cuda/Loss.cu:250: nll_loss_forward_reduce_cuda_kernel_2d: block: [0,0,0], thread: [5,0,0] Assertion `t >= 0 && t < n_classes` failed.
+Traceback (most recent call last):
+  File "/home/matevzvidovic/Desktop/SeminarskaDemo/model.py", line 492, in <module>
+    train(train_dataloader, model, loss_fn, optimizer)
+  File "/home/matevzvidovic/Desktop/SeminarskaDemo/model.py", line 440, in train
+    loss = loss_fn(pred, y)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
+    return self._call_impl(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
+    return forward_call(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/loss.py", line 1179, in forward
+    return F.cross_entropy(input, target, weight=self.weight,
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/functional.py", line 3059, in cross_entropy
+    return torch._C._nn.cross_entropy_loss(input, target, weight, _Reduction.get_enum(reduction), ignore_index, label_smoothing)
+RuntimeError: CUDA error: device-side assert triggered
+Compile with `TORCH_USE_CUDA_DSA` to enable device-side assertions.
+
+
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        """
+        self.linear_relu_stack = nn.Sequential(
+            nn.Linear(300, 300),
+            nn.ReLU(),
+            nn.Linear(300, 300),
+            nn.ReLU(),
+            nn.Linear(300, len(categories))
+        )
+
+    def forward(self, x):
+        x = self.flatten(x)
+        logits = self.linear_relu_stack(x)
+        return logits
+
+model = NeuralNetwork().to(device)
+print(model)
+
+
+
+
+
+
+
+
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-3)
+
+
+
+
+
+
+
+
+
+
+
+def train(dataloader, model, loss_fn, optimizer):
+    size = len(dataloader.dataset)
+    model.train()
+    for batch, (X, y) in enumerate(dataloader):
+
+        """
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+        !!!!!! DODAJ:
+        y = y.type(torch.LongTensor)
+
+
+        TOLE JE STARA NAPAKA, AMPAK JO BOM PUSTIL, DA JE NE RABIM SE ZGORAJ PISAT, KER BI BILO BREZVEZE.
+
+        SEDAJ NAREDIM TO KONERZIJO ZE ZGORAJ, KER SEM PO TRENIRANJU DOBIL
+        RuntimeError: "nll_loss_forward_reduce_cuda_kernel_2d_index" not implemented for 'Char'
+        IN JE BILO KER JE Z Y_TRAIN ENAK PROBLEM
+
+        
+        https://stackoverflow.com/questions/69742930/runtimeerror-nll-loss-forward-reduce-cuda-kernel-2d-index-not-implemented-for
+
+        ker
+        Epoch 1
+-------------------------------
+Traceback (most recent call last):
+  File "/home/matevzvidovic/Desktop/SeminarskaDemo/model.py", line 461, in <module>
+    train(train_dataloader, model, loss_fn, optimizer)
+  File "/home/matevzvidovic/Desktop/SeminarskaDemo/model.py", line 409, in train
+    loss = loss_fn(pred, y)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1511, in _wrapped_call_impl
+    return self._call_impl(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/module.py", line 1520, in _call_impl
+    return forward_call(*args, **kwargs)
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/modules/loss.py", line 1179, in forward
+    return F.cross_entropy(input, target, weight=self.weight,
+  File "/home/matevzvidovic/.local/lib/python3.10/site-packages/torch/nn/functional.py", line 3059, in cross_entropy
+    return torch._C._nn.cross_entropy_loss(input, target, weight, _Reduction.get_enum(reduction), ignore_index, label_smoothing)
+RuntimeError: "nll_loss_forward_reduce_cuda_kernel_2d_index" not implemented for 'Char'
+
+
+        !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        """
+        X, y = X.to(device), y.to(device)
+
+        # Compute prediction error
+        pred = model(X)
+        loss = loss_fn(pred, y)
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if batch % 100 == 0:
+            loss, current = loss.item(), (batch + 1) * len(X)
+            print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
+
+
+
+
+
+
+
+
+
+
+
+
+def test(dataloader, model, loss_fn):
+    size = len(dataloader.dataset)
+    num_batches = len(dataloader)
+    model.eval()
+    test_loss, correct = 0, 0
+    with torch.no_grad():
+        for X, y in dataloader:
+            X, y = X.to(device), y.to(device)
+            pred = model(X)
+            test_loss += loss_fn(pred, y).item()
+            correct += (pred.argmax(1) == y).type(torch.float).sum().item()
+    test_loss /= num_batches
+    correct /= size
+    print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
+
+
+
+
+
+
+
+
+
+
+
+
+
+epochs = 5
+for t in range(epochs):
+    print(f"Epoch {t+1}\n-------------------------------")
+    train(train_dataloader, model, loss_fn, optimizer)
+    test(test_dataloader, model, loss_fn)
+print("Done!")
+
+
+
+
+
+
+
+
+
+
+
+torch.save(model.state_dict(), "model.pth")
+print("Saved PyTorch Model State to model.pth")
+
+
+
+
+
+
+
+
+
+
+
+
+
+model = NeuralNetwork().to(device)
+model.load_state_dict(torch.load("model.pth"))
+
+
+
+
+
+
+
+
+classes = categories
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    x = x.to(device)
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
