377ce1375ba8fe84f0a6015ad99cf5aaf78d5779
x <matevz.vidovic@frida>
2025-01-12 11:26:56 +0000

    Trying jitter as part of data_aug

---
+++ Prototip/Delo/v_add_coye_to_vasd.py
@@ -0,0 +1,44 @@
+
+
+import os
+import os.path as osp
+import numpy as np
+import cv2
+
+import shutil as sh
+
+from PIL import Image
+
+import sys
+
+import matplotlib.pyplot as plt
+
+
+vasd = osp.join("Data", 'vein_and_sclera_data')
+
+bcosfire = osp.join("Data", 'coye2')
+
+
+
+
+
+
+for folder in os.listdir(vasd):
+    goal_f = osp.join(vasd, folder, "Coye")
+    os.makedirs(goal_f, exist_ok=True)
+
+    relevant_imgs = osp.join(vasd, folder, "Images")
+
+    for img_name in os.listdir(relevant_imgs):
+        bcosfire_name = img_name.removesuffix(".jpg") + ".png"
+        curr_source = osp.join(bcosfire, bcosfire_name)
+        curr_dest = osp.join(goal_f, bcosfire_name)
+        try:
+            sh.copy(curr_source, curr_dest)
+        except:
+            print(f"Error with {curr_source} and {curr_dest}")    
+
+
+
+
+sys.exit(0)
